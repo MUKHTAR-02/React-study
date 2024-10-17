b081f8c4cb0e8792d845742567f8eb1e5500b516
Mukhtar <mukhtarkhan9371@gmail.com>
2024-10-17 21:21:10 +0530

    Episode 01

---
+++ ep1/Practice 1/app2.js
@@ -0,0 +1,6 @@
+const heading2 = React.createElement(
+    "h1", {id: "heading2"}, "Hello World From React!, sahi mai"
+);
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(heading2);
